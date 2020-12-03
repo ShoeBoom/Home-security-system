162d5cb8a49f4f7a6619c1ce6d6d6869f06dd5e1
Shubham Bhardwaj <sbhardw8@uwo.ca>
2020-12-02 21:18:31 -0500

    example of onPrediction

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include "UI/mainwindow.h"
 
 #include <QApplication>
+#include <utility>
+#include "demo/subscriptionExample.h"
 
 int main(int argc, char *argv[]) {
+	auto *example = new subscriptionExample();
+	FaceRecognizer::getRecognizer().onPrediction([example](Result r) {
+	  example->run(std::move(r));
+	});
 	QApplication a(argc, argv);
 	MainWindow w;
 	w.show();
